2c782c5dc0afd5658289b37d1ccd309f1601b06a
vicmmont <vicmmonterroso@gmail.com>
2023-04-09 21:56:42 -0400

    s3 signed url

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -7,7 +7,7 @@ export const config = {
     "dialect": "postgres",
     "aws_region": "us-east-1",
     "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "aws_media_bucket": "udagram-monterroso-dev"
   },
   "jwt": {
     "secret": " "
