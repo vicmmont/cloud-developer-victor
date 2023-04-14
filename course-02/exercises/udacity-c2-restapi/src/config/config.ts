552aa6a85285286fc7c121bc6df0ed62b5a5e0ea
vicmmont <vicmmonterroso@gmail.com>
2023-04-13 23:27:32 -0400

    auth

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,16 +1,16 @@
 export const config = {
   "dev": {
-    "username": "udagramvmmdev",
-    "password": "udagramvmmdev",
-    "database": "postgres",
-    "host": "udagramvmmdev.cdz2fhfny31r.us-east-1.rds.amazonaws.com",
+    "username": process.env.POSTGRES_USERNAME,
+    "password": process.env.POSTGRES_PASSWORD,
+    "database": process.env.POSTGRES_DATABASE,
+    "host": process.env.HOST,
     "dialect": "postgres",
-    "aws_region": "us-east-1",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-monterroso-dev"
+    "aws_region": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
   },
   "jwt": {
-    "secret": " "
+    "secret": process.env.JWT_SECRET,
   },
   "prod": {
     "username": "",
