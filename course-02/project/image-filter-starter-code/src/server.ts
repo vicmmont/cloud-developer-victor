0656a86c7072ed6cf828a2f07ac81bed2d4ccfdd
vicmmont <vicmmonterroso@gmail.com>
2023-04-16 16:47:50 -0400

    udagram project

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -17,26 +17,46 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
   // IT SHOULD
-  //    1
   //    1. validate the image_url query
   //    2. call filterImageFromURL(image_url) to filter the image
   //    3. send the resulting file in the response
   //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
+  // QUERY PARAMETERS
   //    image_url: URL of a publicly accessible image
   // RETURNS
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
-  /**************************************************************************** */
+  app.get("/filteredimage", async (req, res) => {
+    const { image_url} = req.query;
+    if (!image_url) {
+      return res.status(400).send("image_url query parameter is required");
+    }
+
+    try {
+      const pathToFile = await filterImageFromURL(image_url);
+
+      return res.sendFile(pathToFile, {}, (err) => {
+        if (err) {
+          console.log('An error occured when sending the file');
+        }
+        else {
+          console.log('File sent successfully');
+        }
+        
+        deleteLocalFiles([pathToFile]);
+      })
+    } catch (error) {
+      return res.status(500).send("There was an error when trying to filter the image");
+    }
+  });
 
   //! END @TODO1
-  
+
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
+    return res.send("try GET /filteredimage?image_url={{}}");
   } );
-  
 
   // Start the Server
   app.listen( port, () => {
